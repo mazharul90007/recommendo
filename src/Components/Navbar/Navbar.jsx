40b39216a8b1aa4d2f95ef8955a5780a004ff8f4
mazharul90007 <mazharul90007@gmail.com>
2024-12-25 22:58:11 +0600

    Navbar Active button system added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,13 +17,17 @@ const Navbar = () => {
     }
 
     const links = <>
-        <NavLink to={'/'}><li>Home</li></NavLink>
-        <NavLink to={'/queries'}><li>Queries</li></NavLink>
+        <NavLink className={({isActive})=> isActive? 'text-green-600 font-bold underline' : 'hover:text-green-600 hover:font-bold'} to={'/'}><li>Home</li></NavLink>
+
+        <NavLink className={({isActive})=> isActive? 'text-green-600 font-bold underline' : 'hover:text-green-600 hover:font-bold'} to={'/queries'}><li>Queries</li></NavLink>
+
         {
             user?.email && <>
-                <NavLink to={'/myQueries'}><li>My Queries</li></NavLink>
-                <NavLink to={'/recommendationForMe'}><li>Recommendation For Me</li></NavLink>
-                <NavLink to={'/myRecommendation'}><li>My Recommendation</li></NavLink>
+                <NavLink className={({isActive})=> isActive? 'text-green-600 font-bold underline' : 'hover:text-green-600 hover:font-bold'} to={'/myQueries'}><li>My Queries</li></NavLink>
+
+                <NavLink className={({isActive})=> isActive? 'text-green-600 font-bold underline' : 'hover:text-green-600 hover:font-bold'} to={'/recommendationForMe'}><li>Recommendation For Me</li></NavLink>
+                
+                <NavLink className={({isActive})=> isActive? 'text-green-600 font-bold underline' : 'hover:text-green-600 hover:font-bold'} to={'/myRecommendation'}><li>My Recommendation</li></NavLink>
             </>
         }
     </>
