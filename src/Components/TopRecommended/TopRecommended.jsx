d62c55d0af26537807d3397dc33a9d6cd7b04b1f
mazharul90007 <mazharul90007@gmail.com>
2025-03-02 21:53:18 +0600

    Top Recommendo page ui updated

--- src/Components/TopRecommended/TopRecommended.jsx
+++ src/Components/TopRecommended/TopRecommended.jsx
@@ -20,7 +20,7 @@ const TopRecommended = () => {
         <div className="w-11/12 mx-auto mb-16">
             <div className="mb-8">
                 <h2 className="text-4xl font-semibold text-green-700 text-center">Top Recommended Products</h2>
-                <p className='text-center text-gray-500 italic'>Discover the best product loved by our users and experts...</p>
+                <p className='text-center text-gray-500 italic'>Discover the best products loved by our users and experts...</p>
                 <div className="divider w-9/12 mx-auto text-4xl text-gray-300"><FaRegStar /></div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
